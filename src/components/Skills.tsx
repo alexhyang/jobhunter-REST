cf0d803b28afb04359580dc58704e4b00ca1917d
Alex Yang <alex.hyang.dev@gmail.com>
2022-08-26 13:53:43 -0700

    completed data fetch

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import WordCloud from "./WordCloud";
+
+async function getSkills() {
+  try {
+    let response = await fetch(
+      "http://alexhyang.herokuapp.com/jobhunter-app/skills/fetch"
+    );
+    return response.json();
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 export default function Skills() {
-  return <div>Skills</div>;
+  const [data, setData] = useState<{ [key: string]: number }>({});
+  useEffect(() => {
+    getSkills().then((value) => setData(value));
+  }, []);
+
+  return (
+    <>
+      <h1>Skills</h1>
+      <WordCloud data={data} />
+    </>
+  );
 }
---
+++ src/components/WordCloud.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {
+  data: {[key: string]: number},
+}
+
+export default function WordCloud(props:Props) {
+  return <></>
+}
