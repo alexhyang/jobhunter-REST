53ec7e3bfff9cdc5ba04df14ff34893ca2178c90
Alex Yang <alex.hyang.dev@gmail.com>
2022-09-07 15:24:12 -0700

    updated server.js

--- server/server.js
+++ server/server.js
@@ -2,11 +2,18 @@ import express from "express";
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log("LOGGED");
+  next();
+};
+
+app.use(logger);
+
 app.get("/", (req, res) => {
   res.send("connected");
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log("Server listening the port http://localhost:" + port);
