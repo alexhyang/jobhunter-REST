0802acc6ae4240f9fbbb17000a68aa2e219c6747
Alex Yang <alex.hyang.dev@gmail.com>
2022-09-08 14:04:36 -0700

    finished Notes page

--- client/src/pages/Notes/Notes.tsx
+++ client/src/pages/Notes/Notes.tsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import React from "react";
 
-export default function Notes() {
-  return <div>Notes</div>;
+interface IListing {
+  [key: string]: string;
+}
+
+export default function Notes(props: { data: IListing[] }) {
+  return (
+    <>
+      <h1>Notes</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Position</th>
+            <th>Notes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.data.map((posting) =>
+            posting.other ? (
+              <tr>
+                <td>{parseInt(posting.id)}</td>
+                <td>
+                  <a href={posting.url}>{posting.position}</a>
+                </td>
+                <td>{posting.other}</td>
+              </tr>
+            ) : null
+          )}
+        </tbody>
+      </table>
+    </>
+  );
 }
