fb7cb72bee6a5b5e1499052a751bd179abd8063e
Alex Yang <alex.hyang.dev@gmail.com>
2022-11-08 16:23:24 -0800

    moved file

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,12 @@
 import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+// console.log(process.env);
 
 const app = express();
+app.use(cors());
 
 const logger = (req, res, next) => {
   console.log("LOGGED");
