b8b5439907e756472ec8ceaeb8bde9a1f1c4836b
Alex Yang <alex.hyang.dev@gmail.com>
2023-07-11 17:58:31 -0700

    add connection to MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -1,21 +1,26 @@
-// Server setup
-require("dotenv").config();
-const port = process.env.PORT || 8000;
-
 const express = require("express");
 const cors = require("cors");
-const logger = require("morgan");
-const postingRoutes = require("./routes/postingRoutes");
+const dotenv = require("dotenv").config();
+const morgan = require("morgan");
+
+const { connect } = require("./db/conn");
+const apiRouter = require("./routes/apiRouter");
 
 const app = express();
-const db = require("./config/db");
+
+// Middleware
 app.use(cors());
-app.use(logger("dev"));
+app.use(express.json());
+app.use(morgan("dev"));
 
 // Routes
-app.use("/api/postings", postingRoutes);
+connect();
+app.use("/api", apiRouter);
+app.use("*", (req, res) => res.status(404).json({ error: "Not Found" }));
 
-app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+
+// Start the server
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`Server listening to port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
