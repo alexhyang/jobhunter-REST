66dbdaed996ab21b4b3cd2b441c28e5c2ac5c27f
Alex Yang <alex.hyang.dev@gmail.com>
2023-07-14 14:28:31 -0700

    improve <Home /> error handling

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -2,19 +2,20 @@ import React, { useState, useEffect } from "react";
 import ListingTable from "./ListingTable";
 
 import { IListing } from "interfaces";
-import { sortListings } from "utils/sortListings";
-import { fetchPostings } from "utils/APIs";
 
-export default function Home() {
-  const [data, setData] = useState<IListing[]>();
-  useEffect(() => {
-    fetchPostings().then((listings) => setData(sortListings(listings)));
-  }, []);
-
+export default function Home(props: { data: IListing[] | undefined }) {
   return (
     <>
-      <h1>Postings List {data ? `(${data.length} in total)` : null}</h1>
-      {data ? <ListingTable data={data} /> : <p>Loading...</p>}
+      <h1>
+        Postings List {props.data ? `(${props.data.length} in total)` : null}
+      </h1>
+      {props.data == undefined ? (
+        <p>No Posting found</p>
+      ) : props.data ? (
+        <ListingTable data={props.data} />
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   );
 }
