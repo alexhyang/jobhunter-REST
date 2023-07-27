fb23b0ca16b533e715dc2ae60579f30c096297ef
Alex Yang <alex.hyang.dev@gmail.com>
2023-07-27 16:34:45 -0700

    finish summaries API endpoint

--- controllers/SummaryController.js
+++ controllers/SummaryController.js
@@ -2,19 +2,37 @@ const Posting = require("../models/Posting");
 
 const SummaryController = {
   getSkills: async (req, res) => {
+    const { option } = req.query;
+    if (option !== undefined && option === "unique") {
+      try {
+        const postings = await Posting.distinct("skills");
+        return res.status(200).json(postings);
+      } catch (error) {
+        console.error("Error retrieving unique skills:", error);
+        return res.status(500).json({ error: "Failed to retrieve unique skills" });
+      }
+    }
+
     const projectSkills = { $project: { skills: 1 } };
-    const projectName = { $project: { _id: 1 } };
     const unwindStage = { $unwind: "$skills" };
     const groupStage = { $group: { _id: "$skills", count: { $count: {} } } };
+    const sortStage = { $sort: { count: -1 } };
+    const limitStage = { $limit: 30 };
+
+    const aggregateStages = [
+      projectSkills,
+      unwindStage,
+      groupStage,
+      sortStage,
+      limitStage
+    ];
 
     try {
-      const postings = await Posting.aggregate([
-        projectSkills, unwindStage, groupStage, projectName
-      ]);
+      const postings = await Posting.aggregate(aggregateStages);
       res.status(200).json(postings);
     } catch (error) {
-      console.error("Error retrieving postings:", error);
-      res.status(500).json({ error: "Failed to retrieve postings" });
+      console.error("Error retrieving skills count:", error);
+      res.status(500).json({ error: "Failed to retrieve skills count" });
     }
   },
 
@@ -22,7 +40,7 @@ const SummaryController = {
     try {
       const postings = await Posting.find(
         { other: { $ne: "" } },
-        { jobLevel: 1, other: 1 }
+        { jobTitle: 1, jobLevel: 1, other: 1 }
       ).sort({ applicationDueDate: -1 });
       res.status(200).json(postings);
     } catch (error) {
