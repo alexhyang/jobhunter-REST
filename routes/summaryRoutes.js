998c948bfc423b97db4b1f28592bcc649f47814b
Alex Yang <alex.hyang.dev@gmail.com>
2023-07-29 00:37:38 -0700

    add skill count endpoint for API

--- routes/summaryRoutes.js
+++ routes/summaryRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const SummaryController = require("../controllers/SummaryController")
+const SummaryController = require("../controllers/SummaryController");
 
 router.get("/skills", SummaryController.getSkills);
+router.get("/skills/count", SummaryController.getSkillCounts);
 router.get("/notes", SummaryController.getNotes);
 
 module.exports = router;
